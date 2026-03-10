5ddd0b49258850aba5aee9149656028aa4f32278
Alexander Phillips <alexphillips-dev@users.noreply.github.com>
2026-03-09 23:17:25 -0400

    Improve Unraid update detection with cache-busted manifest URLs

--- src/plexstreamsplus/usr/local/emhttp/plugins/plexstreamsplus/includes/common.php
+++ src/plexstreamsplus/usr/local/emhttp/plugins/plexstreamsplus/includes/common.php
@@ -2,7 +2,7 @@
     if (isset($GLOBALS['unRaidSettings'])) {
         define('OS_VERSION', 'Unraid ' . $GLOBALS['unRaidSettings']['version']);
     }
-    define('PLUGIN_VERSION', '2026.03.09.23');
+    define('PLUGIN_VERSION', '2026.03.09.25');
 
     function normalizeHostUrl($host) {
         $host = trim((string)$host);
