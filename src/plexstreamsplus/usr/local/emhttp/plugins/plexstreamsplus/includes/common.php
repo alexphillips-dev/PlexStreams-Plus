b2a927098a37b69b4a6a11eded9ae18a5e5471eb
Alexander Phillips <alexphillips-dev@users.noreply.github.com>
2026-03-09 20:20:35 -0400

    Bump to 2026.03.10.12 and simplify install scripts for update reliability

--- src/plexstreamsplus/usr/local/emhttp/plugins/plexstreamsplus/includes/common.php
+++ src/plexstreamsplus/usr/local/emhttp/plugins/plexstreamsplus/includes/common.php
@@ -2,7 +2,7 @@
     if (isset($GLOBALS['unRaidSettings'])) {
         define('OS_VERSION', 'Unraid ' . $GLOBALS['unRaidSettings']['version']);
     }
-    define('PLUGIN_VERSION', '2026.03.10.11');
+    define('PLUGIN_VERSION', '2026.03.10.12');
 
     function normalizeHostUrl($host) {
         $host = trim((string)$host);
