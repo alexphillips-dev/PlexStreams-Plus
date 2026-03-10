8872c217237055aec4914786925459def8ad6b7b
Alexander Phillips <alexphillips-dev@users.noreply.github.com>
2026-03-09 20:12:23 -0400

    Restore legacy update path and config migration for Unraid pulls

--- src/plexstreamsplus/usr/local/emhttp/plugins/plexstreamsplus/includes/config.php
+++ src/plexstreamsplus/usr/local/emhttp/plugins/plexstreamsplus/includes/config.php
@@ -2,9 +2,13 @@
     $plugin = "plexstreamsplus";
     $plg_path = "/boot/config/plugins/" . $plugin;
     $cfg_file    = "$plg_path/" . $plugin . ".cfg";
+    $legacy_cfg_file = "/boot/config/plugins/plexstreams/plexstreams.cfg";
 
     if (file_exists($cfg_file)) {
         $cfg  = parse_ini_file($cfg_file);
+    } else if (file_exists($legacy_cfg_file)) {
+        // Migration fallback for installs that were previously "plexstreams".
+        $cfg  = parse_ini_file($legacy_cfg_file);
     } else {
         $cfg = array();
     }
