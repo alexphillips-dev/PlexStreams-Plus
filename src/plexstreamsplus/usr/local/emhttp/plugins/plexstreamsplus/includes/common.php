c5ad6cf117f37a08941dad10526021b59747bf6d
Alexander Phillips <alexphillips-dev@users.noreply.github.com>
2026-03-30 19:04:03 -0400

    release: 2026.03.30.2

--- src/plexstreamsplus/usr/local/emhttp/plugins/plexstreamsplus/includes/common.php
+++ src/plexstreamsplus/usr/local/emhttp/plugins/plexstreamsplus/includes/common.php
@@ -2,7 +2,7 @@
     if (isset($GLOBALS['unRaidSettings'])) {
         define('OS_VERSION', 'Unraid ' . $GLOBALS['unRaidSettings']['version']);
     }
-    define('PLUGIN_VERSION', '2026.03.30.1');
+    define('PLUGIN_VERSION', '2026.03.30.2');
 
     if (!function_exists('getInstalledPluginVersion')) {
         function getInstalledPluginVersion($fallbackVersion) {
